314e4d8a822af72f1ff98b1a70b3b2392ffd0b4a
MiguelJimenez023 <migueeel11.7@gmail.com>
2016-04-29 20:03:48 +0200

    K

---
+++ PROJECTE.SIRENA.3/PROJECTE.SIRENA.3.ino
@@ -0,0 +1,23 @@
+int durada = 9;
+const int potenciometre0 = A0;
+const int potenciometre1 = A1;
+int valPot0;
+int valPot1;
+int freq;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(durada,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+valPot0= analogRead (potenciometre0)*10;
+valPot1= analogRead (potenciometre1)*10;
+
+
+tone (durada,valPot0,valPot1);
+delay(2*valPot1);
+
+
+}
