90e7d394f444239f39fa5af08ecfa768f511317f
MiguelJimenez023 <migueeel11.7@gmail.com>
2016-05-19 14:21:58 +0200

    h

---
+++ avisadoracustic/avisadoracustic.ino
@@ -0,0 +1,36 @@
+int durada = 9;
+const int LDR1 = A0;
+const int LDR2 = A1;
+const int LDR3 = A2;
+int valLdr0;
+int valLdr1;
+int valLdr2;
+int freq;
+int comparacio;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(durada,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+valLdr0= analogRead (LDR1);
+valLdr1= analogRead (LDR2);
+valLdr2= analogRead (LDR3);
+comparacio=1024;
+
+comparacio=min(valLdr0,valLdr1);
+
+//comparacio=min(valLdr2,comparacio);
+
+
+//PONER LA SUMA DE LOS TRES Y DIVIDIRLOS ENTRE 3
+Serial.println(comparacio);
+tone (durada,1000,comparacio);
+delay(2*comparacio);
+
+}
---
+++ mi_frase/mi_frase.ino
@@ -0,0 +1,110 @@
+/******************************************************************************
+**                                                                           **
+**                              Morse                                        **
+**                                                                           **
+**                                                                           **
+******************************************************************************/
+
+//****** Includes *************************************************************
+
+//****** Variables ************************************************************
+// pin for speaker
+const int pin = 9;
+
+int note = 1047;
+int velocitat = 10;
+
+//****** Setup ****************************************************************
+void setup()
+{
+}
+
+//****** Loop *****************************************************************
+void loop()
+{
+  //H   ** Exemple hijo puta **
+  punt();
+  punt();
+  punt();
+  punt();
+  //Espai entre lletres
+  espaiL();
+  //I
+  punt();
+  punt();
+  
+  //Espai entre lletres
+  espaiL();
+  //J
+  punt();
+  ratlla();
+  ratlla();
+  ratlla();
+  
+  //Espai entre lletres
+  espaiL();
+
+  //O
+  ratlla();
+  ratlla();
+  ratlla();
+
+  //Espai entre paraules
+  espaiP();
+
+  //P
+  punt();
+  ratlla();
+  ratlla();
+  punt();
+
+//Espai entre lletres
+  espaiL();
+  
+  //U
+  punt();
+  punt();
+  ratlla();
+
+  //Espai entre lletres
+  espaiL();
+
+  //T
+  ratlla();
+
+  //Espai entre lletres
+  espaiL();
+
+  //A
+ punt();
+  ratlla();
+  
+ //Espai entre paraules
+  espaiP(); 
+ 
+}
+
+//****** Funcions *************************************************************
+void punt()
+{
+  tone(pin, note, 1000/velocitat);
+  delay(1000/velocitat); 
+  delay(1000/velocitat); 
+}
+
+void ratlla()
+{
+  tone(pin, note, 3*(1000/velocitat));
+  delay(3*(1000/velocitat)); 
+  delay(1000/velocitat);  
+}
+
+void espaiL()  //Espai entre lletres
+{  
+  delay(2*(1000/velocitat));  
+}
+
+void espaiP()  //Espai entre paraules
+{   
+  delay(6*(1000/velocitat));  
+}
